00e44e6405d000454ac5821d87ac68384bb1ad2c
Sam Hodak <slhodak@gmail.com>
2019-07-22 18:23:49 -0700

    Add DAW class

--- client/src/daw.js
+++ client/src/daw.js
@@ -1,8 +1,18 @@
 //  Browser-Based DAW
 //  Brings synths and sequencer together (and other tools)
 
+//  require subcomponents accessible via network?
 const Manager = {
+  daw: null,
+  createDAWIfNoneExists() {
+    Manager.daw = new DAW();
+  },
   darkMode: false
+};
+
+const DAW = function() {
+  this.synthesizers = [];
+  this.pianoRoll = null;
 };
 
 export default Manager;
