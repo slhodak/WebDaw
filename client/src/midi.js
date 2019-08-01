ea83e62ee2cc253bed7843560ed04efc9cfb319f
Sam Hodak <slhodak@gmail.com>
2019-08-01 11:56:57 -0700

    Enable midi input to synthesizers in daw

---
+++ client/src/midi.js
@@ -0,0 +1,29 @@
+import DawManager from './daw.js';
+
+let MIDIKeyboard = {
+  connect() {
+    window.navigator.requestMIDIAccess()
+      .then(midiAccess => {
+        MIDIKeyboard.create(midiAccess);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  },
+  create(midiAccess) {
+    MIDIKeyboard.midiAccess = midiAccess;
+    console.log('connected to midi');
+    midiAccess.inputs.forEach(port => {
+      port.onmidimessage = (message) => {
+        if (DawManager.daw) {
+          MIDIKeyboard.handleInput(message);
+        }
+      }
+    });
+  },
+  handleInput(message) {
+    DawManager.daw.handleMIDI(message);
+  }
+};
+
+MIDIKeyboard.connect();
