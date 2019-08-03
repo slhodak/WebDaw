b555c9939c1c4428c062c6bf227326fde78a9fe0
Sam Hodak <slhodak@gmail.com>
2019-08-03 13:10:38 -0700

    Create synth template for initializing new synths

---
+++ client/src/lib/newSynth.js
@@ -0,0 +1,24 @@
+export default {
+  "name": null,
+  "router": {
+    "1000": "main out",
+  },
+  "settings": {
+    "poly": true,
+    "globals": {
+      "porta": 0.05,
+      "attack": 0.01,
+      "release": 0.1
+    }
+  },
+  "oscillators": [
+    {
+      "id": 1000,
+      "semitoneOffset": 0,
+      "fineDetune": 0,
+      "volume": 0.75,
+      "type": "sine"
+    }
+  ],
+  "filters": []
+};
